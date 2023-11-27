c53ffee7ab222f9ab08811fd22d753c3a07fa251
John Yuill <john@fig4.com>
2023-11-26 16:25:24 -0800

    proper file name, some basics

---
+++ 01-basics.py
@@ -0,0 +1,22 @@
+## intro examples
+# basic printing
+print("Whatcha got now?")
+# print with concatenate
+print("Whatcha got "+"right now"+"!?")
+# example with ''
+print("The so-called 'currency'.")
+
+# input - make sure running in Terminal 
+# with project path % python <filename>
+# NOT just running here - will appear in Output pane but no interactivity
+# demo - nothing happens
+input("Hey, what is your name, anyway? ")
+# input saved as variable
+yname = input("name please: ")
+# print results from input
+print("hello "+yname+" mucho gusto!")
+
+
+
+
+
